98ad1cfaf668d56a7e225583b979dec0db2602f9
Andri Priyadi <dripriyadi@gmail.com>
2023-12-22 15:40:11 +0700

    post not tested

--- routes/todos.js
+++ routes/todos.js
@@ -4,25 +4,30 @@ var router = express.Router();
 
 
 module.exports = function (db) {
-  const User = db.collection('todos')
+    const Todo = db.collection('todos')
+    const User = db.collection('users')
+    //   router.get('/', async function(req,res,next){
+    //     try {
+    //         const { page = 1, limit = 5, query = '', sortBy, sortMode } = req.query
+    //       const params = {}
+    //       const sort = {}
+    //       sort[sortBy] = sortMode
+    //       const offset = (page - 1) * limit
+    //     } catch (error) {
 
-//   router.get('/', async function(req,res,next){
-//     try {
-//         const { page = 1, limit = 5, query = '', sortBy, sortMode } = req.query
-//       const params = {}
-//       const sort = {}
-//       sort[sortBy] = sortMode
-//       const offset = (page - 1) * limit
-//     } catch (error) {
-        
-//     }
-//   })
+    //     }
+    //   })
 
-    router.post('/', async function(req,res){
+    router.post('/', async function (req, res) {
         try {
-            const {title, executor} = req.body
+            const { title, executor } = req.body
+            const user = await User.findOne({ _id: executor })
+            const todo = await Todo.insertOne({ title: title, complete: false, executor: new ObjectId(user._id) })
+            res.json(todo)
         } catch (error) {
-            
+            res.status(500).json({ error: error.message })
+
         }
     })
+
 }
