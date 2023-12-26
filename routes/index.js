f7d145bb00066430cf080096794faec92b1cd989
Andri Priyadi <dripriyadi@gmail.com>
2023-12-26 22:08:37 +0700

    starting views

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('users');
 });
-
+router.get('/users/:id/todos', function(req,res,next){
+  res.render('todos', {executor : req.params.id})
+})
 module.exports = router;
